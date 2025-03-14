6b61684168d0e3d7d568877151b1f2d33732ab83
Abbie Gorlov-Webster <AbbiegorlovWebster@gmail.com>
2025-03-14 10:45:25 +0000

    Font Update
    Corrects misnamed fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,8 @@ import { useFonts } from "expo-font";
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
-    Puritan: require("@/assets/fonts/Puritan-Bold.ttf"),
-    PuritanBold: require("@/assets/fonts/Puritan-Regular.ttf"),
+    Puritan: require("@/assets/fonts/Puritan-Regular.ttf"),
+    PuritanBold: require("@/assets/fonts/Puritan-Bold.ttf"),
     PuritanBoldItalic: require("@/assets/fonts/Puritan-BoldItalic.ttf"),
     PuritanItalic: require("@/assets/fonts/Puritan-Italic.ttf"),
   });
